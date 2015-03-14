41fd8a936c7185e855d1a1b303518e103601496d
Bruno Sutic <bruno.sutic@gmail.com>
2015-03-14 23:18:34 +0100

    Small refactor

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -14,23 +14,26 @@ default_save_complete_history_key="M-P" # Alt-Shift-p
 clear_history_key_option="@clear-history-key"
 default_clear_history_key="M-c" # Alt-c
 
+# General options
+filename_suffix="#{session_name}-#{window_index}-#{pane_index}-%Y%m%dT%H%M%S.log"
+
 # Logging options
 logging_path_option="@logging-path"
 default_logging_path="$HOME"
 
 logging_filename_option="@logging-filename"
-default_logging_filename="tmux-#{session_name}-#{window_index}-#{pane_index}-%Y%m%dT%H%M%S.log"
+default_logging_filename="tmux-${filename_suffix}"
 
 # Screen capture options
 screen_capture_path_option="@screen-capture-path"
 default_screen_capture_path="$HOME"
 
 screen_capture_filename_option="@screen-capture-filename"
-default_screen_capture_filename="tmux-screen-capture-#{session_name}-#{window_index}-#{pane_index}-%Y%m%dT%H%M%S.log"
+default_screen_capture_filename="tmux-screen-capture-${filename_suffix}"
 
 # Save complete history options
 save_complete_history_path_option="@save-complete-history-path"
 default_save_complete_history_path="$HOME"
 
 save_complete_history_filename_option="@save-complete-history-filename"
-default_save_complete_history_filename="tmux-history-#{session_name}-#{window_index}-#{pane_index}-%Y%m%dT%H%M%S.log"
+default_save_complete_history_filename="tmux-history-${filename_suffix}"
