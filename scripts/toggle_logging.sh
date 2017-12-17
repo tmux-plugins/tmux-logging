893e46c0cf371bda460562bc57dfe60de0b55b62
Sergey Yakovlev <selfuryon@gmail.com>
2017-12-18 02:18:08 +0500

    Adding capability to autocreate folders in logging paths if they don't exist

--- scripts/toggle_logging.sh
+++ scripts/toggle_logging.sh
@@ -4,15 +4,17 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/variables.sh"
 source "$CURRENT_DIR/shared.sh"
+source "$CURRENT_DIR/capture_pane_helpers.sh"
 
-get_filename() {
-	local logging_path="$(get_tmux_option "$logging_path_option" "$default_logging_path")"
-	local logging_filename="$(get_tmux_option "$logging_filename_option" "$default_logging_filename")"
-	echo "${logging_path}/${logging_filename}"
-}
+# Functions defined in capture_pane_helpers are customized via below variables.
+default_path="$default_logging_path"
+default_name="$default_logging_filename"
+
+path_option="$logging_path_option"
+name_option="$logging_filename_option"
 
 start_pipe_pane() {
-	local filename="$(get_filename)"
+	local filename="$(get_path)/$(get_filename)"
 	"$CURRENT_DIR/start_logging.sh" "$filename"
 	display_message "Started logging to $filename"
 }
